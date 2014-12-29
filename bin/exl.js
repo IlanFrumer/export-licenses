230aad0cdb5938b64f72dee633fbb35284e57dc2
Ilan Frumer <ilanfrumer@gmail.com>
2014-12-29 11:02:45 +0200

    added csv and version , fallback licenses fix

--- bin/exl.js
+++ bin/exl.js
@@ -9,13 +9,13 @@ var FLAGS = [
   { flag: 'npm',     alias: 'n', desc: 'get npm dependencies liscenses' },
   { flag: 'bower',   alias: 'b', desc: 'get bower dependencies liscenses' },
   { flag: 'dev',     alias: 'd', desc: 'also get liscenses of devDependencies' },
-  { flag: 'check',   alias: 'c', desc: 'validate dependencies urls with http requests (requires network connection)' },
   { flag: 'help',    alias: 'h', desc: 'display help screen' },
   { flag: 'json',    alias: 'j', desc: 'print output as a json object' },
+  { flag: 'csv',     alias: 'c', desc: 'print output as csv' },
   { flag: 'silence', alias: 's', desc: 'suppress output' },
-  { flag: 'compact', alias: 't', desc: 'print table in compact table' },
   { flag: 'sort'   , alias: 'a', desc: 'print table sorted by license type' },
-  { flag: 'version', alias: 'v', desc: 'display the version number' }
+  { flag: 'version', alias: 'v', desc: 'display the version number' },
+  { flag: 'verify',  alias: 'V', desc: 'validate dependencies urls with http requests (requires network connection)' }
 ];
 
 var flags = [];
@@ -36,14 +36,58 @@ var argv = minimist(process.argv.slice(2), {
   }
 });
 
+var print = function(title, message) {
+  log('\n' + colors.magenta(title + ': ') + colors.grey(message));
+};
+
 if (unknown.length > 0) {
-  return log('\n' + colors.magenta('Unknown flags:') + ' ' , colors.grey(unknown.join(' ')));
+  return print('Unknown flags',  unknown.join(' '));
 }
 
+if(argv.version) {
+  return print('export-licenses',  'version ' + require('../package.json').version);
+}
 
-var checkPrintLicenses = function(licenses) {
-  if (argv.check) {
-    api.check(licenses , function(licenses) {
+if (argv.help) {
+  var Table = require('cli-table');
+  var table = new Table({
+    chars: {
+      'top': '' ,
+      'top-mid': '' ,
+      'top-left': '' ,
+      'top-right': '',
+      'bottom': '' ,
+      'bottom-mid': '' ,
+      'bottom-left': '' ,
+      'bottom-right': '',
+      'left': '' ,
+      'left-mid': '' ,
+      'mid': '' ,
+      'mid-mid': '',
+      'right': '' ,
+      'right-mid': '',
+      'middle': ' '
+    },
+    style: {
+      'padding-left': 0,
+      'padding-right': 2,
+      compact : true
+    }
+  });
+
+  FLAGS.forEach(function(item) {
+    table.push([ '--' + item.flag, '-' +item.alias, item.desc]);
+  });
+
+  print('Usage', 'exl <flags> [folder]');
+  log('');
+  log(table.toString());
+  return;
+}
+
+var verifyPrintLicenses = function(licenses) {
+  if (argv.verify) {
+    api.verify(licenses , function(licenses) {
       if(!argv.silence) {
         api.print(licenses, argv);
       }
@@ -56,26 +100,8 @@ var checkPrintLicenses = function(licenses) {
 var api = require('../index.js');
 var dir = argv._[0] || '.';
 
-if(argv.version) {
-  log(require('../package.json').version);
-} else if (argv.help) {
-  var Table = require('cli-table');
-  var table = new Table({
-    style: {
-      'padding-left': 1,
-      'padding-right': 1,
-      compact : true
-    }
-  });
-
-  FLAGS.forEach(function(item) {
-    table.push([ '--' + item.flag, '-' +item.alias, item.desc]);
-  });
-
-  log('\nUsage: exl <flags> [folder]');
-  log(table.toString());
-} else if (argv.npm || !argv.bower) {
-  checkPrintLicenses(api.npm(dir, argv.dev));
+if (argv.npm || !argv.bower) {
+  verifyPrintLicenses(api.npm(dir, argv.dev));
 } else if (argv.bower) {
-  checkPrintLicenses(api.bower(dir, argv.dev));
+  verifyPrintLicenses(api.bower(dir, argv.dev));
 }
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var api = {
   matcher: require('./lib/licenseMatcher.js'),
   search: require('./lib/licenseSearch.js'),
   meta: require('./lib/metaJSON.js'),
-  check: require('./lib/checkLicenses'),
+  verify: require('./lib/verifyLicenses'),
   print: require('./lib/printLicenses'),
   bower: require('./lib/searchBower.js'),
   npm: require('./lib/searchNpm.js')
